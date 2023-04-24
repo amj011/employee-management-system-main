92f670f72b4912c92091be9a1036242c0ff92c76
Vedant Gokhale <gokhalevedant06@gmail.com>
2023-04-24 10:55:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-        return [
-          {
-            source: 'https://employee-management-system-main-4eua.vercel.app/:path*',
-            destination: 'https://employee-management-system-main-4eua.vercel.app/:path*',
-          },
-        ]
-      },
 }
 
 module.exports = nextConfig
